44ee5ecf91f40b26126f8e1c7f5b021d4a5ab638
Chaz <790@sicksubroutine.com>
2023-01-21 22:04:20 +0000

    push

--- main.py
+++ main.py
@@ -63,8 +63,6 @@ def tempNow(city):
     current_temperature_f = round(current_temperature * (9 / 5) - 459.67)
     print(f"The current temperature in {use.cConvert('red')}{city}{use.cConvert(None)} is {current_temperature_f}°F")
 
-
-
 if __name__ == "__main__":
     tempNow = tempNow(city)
     print()
